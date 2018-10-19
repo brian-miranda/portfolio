7d44078695d8c21ccbc9942590e25633b133ed3b
Brian Miranda <brianmiranda@outlook.com>
2018-10-19 18:32:28 -0500

    Major JQuery modal fix. MAJOR pain in the butt

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,43 +1,51 @@
 $(document).ready(function() {
 
   $(function(){
-  $('#header_nav').data('size','big');
+  $("#header_nav").data("size","big");
   });
 
   $(window).scroll(function(){
     if($(document).scrollTop() > 0)
   {
-      if($('#header_nav').data('size') == 'big')
+      if($("#header_nav").data("size") == "big")
       {
-          $('#header_nav').data('size','small');
-          $('#header_nav').stop().animate({
-            height:'80px'
+          $("#header_nav").data("size","small");
+          $("#header_nav").stop().animate({
+            height:"80px"
           },400);
-          $('.logo img').css('height', '40');
-          $('.logo').css('margin-top', '20px');
-          $('.logo img').css('transform', 'rotate(270deg)');
-          $('.brand').fadeOut();
-          $('.sidenav div').css('text-align', 'left');
-          $('.sidenav div').css('width', '70px');
+          $(".logo img").css("height", "40");
+          $(".logo").css("margin-top", "20px");
+          $(".logo img").css("transform", "rotate(270deg)");
+          $(".brand").fadeOut();
+          $(".sidenav div").css("text-align", "left");
+          $(".sidenav div").css("width", "70px");
       }
   }
   else
     {
-      if($('#header_nav').data('size') == 'small')
+      if($("#header_nav").data("size") == "small")
         {
-          $('#header_nav').data('size','big');
-          $('#header_nav').stop().animate({
-            height:'160px'
+          $("#header_nav").data("size","big");
+          $("#header_nav").stop().animate({
+            height:"160px"
           },100);
-          $('.logo img').css('height', '80');
-          $('.logo').css('margin', '40px');
-          $('.logo img').css('transform', 'rotate(0deg)');
-          $('.brand').fadeIn();
-          $('.sidenav div').css('text-align', 'left');
-          $('.sidenav div').css('width', '150px');
+          $(".logo img").css("height", "80");
+          $(".logo").css("margin", "40px");
+          $(".logo img").css("transform", "rotate(0deg)");
+          $(".brand").fadeIn();
+          $(".sidenav div").css("text-align", "left");
+          $(".sidenav div").css("width", "150px");
         }
     }
   });
+
+  $('a.box').click(function(event) {
+      $(this).modal({
+        fadeDuration: 1000
+      });
+      return false;
+    });
+
 });
 
 // Change heading
