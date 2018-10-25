ef29aef73b595dbaf77b8c73d61eca1a772ed3e5
Brian Miranda <brianmiranda@outlook.com>
2018-10-24 19:09:23 -0500

    Add some font js and php, but still not working

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-
+  // header animation
   $(function(){
   $("#header_nav").data("size","big");
   });
@@ -39,6 +39,7 @@ $(document).ready(function() {
     }
   });
 
+  // modal stuff
   $('a.box').click(function(event) {
       $(this).modal({
         fadeDuration: 500,
@@ -46,6 +47,88 @@ $(document).ready(function() {
       return false;
     });
 
+  // contact form
+  var errors = false;
+    $('.required').parent().find('.input').on('blur', function() {
+        var error_div = $(this).parent().find('.error_message');
+        var field_container = $(this).parent();
+        if (!$.empty_field_validation($(this).val())) {
+            error_div.html('This field is required.');
+            error_div.css('display', 'block');
+            field_container.addClass('error');
+			errors = true;
+        } else {
+            error_div.html('');
+            error_div.css('display', 'none');
+            field_container.removeClass('error');
+			errors = false;
+        }
+    });
+    $('#email').on('blur', function(){
+        var error_div = $(this).parent().find('.error_message');
+        var field_container = $(this).parent();
+        if (!$.email_validation($(this).val())) {
+            error_div.html('Expected Input: email');
+            error_div.css('display', 'block');
+            field_container.addClass('error');
+			errors = true;
+        } else {
+            error_div.html('');
+            error_div.css('display', 'none');
+            field_container.removeClass('error');
+			errors = false;
+        }
+    });
+	$('#contact_form').submit(function(event) {
+		event.preventDefault();
+		 $('.required').parent().find('.input').trigger('blur');
+        if (!errors)
+            $.ajax({
+                url: '/echo/json/',
+                data: {
+                    json: JSON.stringify($(this).serializeObject())
+                },
+                type: 'post',
+                success: function(data) {
+                    var message = 'Hi '+data.name+'. Your message was sent and received.';
+                    $('#after_submit').html(message);
+                    $('#after_submit').css('display', 'block');
+                },
+                error: function() {
+                    var message = 'Hi '+data.name+'. Your message could not be sent or received. Please try again later';
+                    $('#after_submit').html(message);
+                    $('#after_submit').css('display', 'block');
+                }
+            });
+		else
+			alert("You didn't completed the form correctly. Check it out and try again!");
+	});
+
 });
 
-// Change heading
+// contact form validation or some shit
+$.empty_field_validation = function(field_value) {
+    if (field_value.trim() == '') return false;
+    return true;
+}
+
+$.email_validation = function(email) {
+    var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    return regex.test(email);
+}
+$.fn.serializeObject = function()
+{
+   var o = {};
+   var a = this.serializeArray();
+   $.each(a, function() {
+       if (o[this.name]) {
+           if (!o[this.name].push) {
+               o[this.name] = [o[this.name]];
+           }
+           o[this.name].push(this.value || '');
+       } else {
+           o[this.name] = this.value || '';
+       }
+   });
+   return o;
+};
