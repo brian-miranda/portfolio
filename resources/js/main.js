25eea7d676bfd0f14eed20a1ce11f4f5da50f98e
Brian Miranda <brianmiranda@outlook.com>
2018-11-28 13:53:57 -0600

    Start mobile version ouch

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
   // header animation
   $(function(){
-  $("#header_nav").data("size","big");
+  $("#header-nav").data("size","big");
   });
 
   $(window).scroll(function(){
     if($(document).scrollTop() > 0)
   {
-      if($("#header_nav").data("size") == "big")
+      if($("#header-nav").data("size") == "big")
       {
-          $("#header_nav").data("size","small");
-          $("#header_nav").stop().animate({
+          $("#header-nav").data("size","small");
+          $("#header-nav").stop().animate({
             height:"80px"
           },400);
           $(".logo img").css("height", "40");
@@ -23,10 +23,10 @@ $(document).ready(function() {
   }
   else
     {
-      if($("#header_nav").data("size") == "small")
+      if($("#header-nav").data("size") == "small")
         {
-          $("#header_nav").data("size","big");
-          $("#header_nav").stop().animate({
+          $("#header-nav").data("size","big");
+          $("#header-nav").stop().animate({
             height:"160px"
           },100);
           $(".logo img").css("height", "80");
