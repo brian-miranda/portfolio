d80aa0b1905849806bc850632837a3ca7dd555c7
Brian Miranda <brianmiranda@outlook.com>
2019-07-02 20:55:08 -0500

    Fix minor typos, add minor descriptions, beautify code. Daddy's back bitches

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,50 +1,45 @@
 $(document).ready(function() {
   // header animation
-  $(function(){
-  $("#header-nav").data("size","big");
+  $(function() {
+    $("#header-nav").data("size", "big");
   });
 
-  $(window).scroll(function(){
-    if($(document).scrollTop() > 0)
-  {
-      if($("#header-nav").data("size") == "big")
-      {
-          $("#header-nav").data("size","small");
-          $("#header-nav").stop().animate({
-            height:"80px"
-          },400);
-          $(".logo img").css("height", "40");
-          $(".logo").css("margin-top", "20px");
-          $(".logo img").css("transform", "rotate(270deg)");
-          $(".brand").fadeOut();
-          $(".sidenav div").css("text-align", "left");
-          $(".sidenav div").css("width", "70px");
+  $(window).scroll(function() {
+    if ($(document).scrollTop() > 0) {
+      if ($("#header-nav").data("size") == "big") {
+        $("#header-nav").data("size", "small");
+        $("#header-nav").stop().animate({
+          height: "80px"
+        }, 400);
+        $(".logo img").css("height", "40");
+        $(".logo").css("margin-top", "20px");
+        $(".logo img").css("transform", "rotate(270deg)");
+        $(".brand").fadeOut();
+        $(".sidenav div").css("text-align", "left");
+        $(".sidenav div").css("width", "70px");
       }
-  }
-  else
-    {
-      if($("#header-nav").data("size") == "small")
-        {
-          $("#header-nav").data("size","big");
-          $("#header-nav").stop().animate({
-            height:"160px"
-          },100);
-          $(".logo img").css("height", "80");
-          $(".logo").css("margin", "40px");
-          $(".logo img").css("transform", "rotate(0deg)");
-          $(".brand").fadeIn();
-          $(".sidenav div").css("text-align", "left");
-          $(".sidenav div").css("width", "150px");
-        }
+    } else {
+      if ($("#header-nav").data("size") == "small") {
+        $("#header-nav").data("size", "big");
+        $("#header-nav").stop().animate({
+          height: "160px"
+        }, 100);
+        $(".logo img").css("height", "80");
+        $(".logo").css("margin", "40px");
+        $(".logo img").css("transform", "rotate(0deg)");
+        $(".brand").fadeIn();
+        $(".sidenav div").css("text-align", "left");
+        $(".sidenav div").css("width", "150px");
+      }
     }
   });
 
   // modal stuff
   $('a.box').click(function(event) {
-      $(this).modal({
-        fadeDuration: 500,
-      });
-      return false;
+    $(this).modal({
+      fadeDuration: 500,
+    });
+    return false;
   });
 
   // scroll animated buttons
